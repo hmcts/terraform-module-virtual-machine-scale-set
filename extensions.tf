9e9250df306cf3252f486707b2e424a619fb4d9b
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-06-29 12:10:34 +0100

    added virtual_machine_scale_set_id

--- extensions.tf
+++ extensions.tf
@@ -3,19 +3,19 @@ module "vm-bootstrap" {
   count  = var.install_splunk_uf == true || var.nessus_install == true ? 1 : 0
   source = "git::https://github.com/hmcts/terraform-module-vm-bootstrap.git?ref=master"
 
-  virtual_machine_type       = "vmss"
-  virtual_machine_id         = var.vm_type == "linux-scale-set" ? azurerm_linux_virtual_machine_scale_set.linux_scale_set[0].id : azurerm_windows_virtual_machine_scale_set.windows_scale_set[0].id
-  splunk_username            = var.splunk_username
-  splunk_password            = var.splunk_password
-  splunk_pass4symmkey        = var.splunk_pass4symmkey
-  splunk_group               = var.splunk_group
-  os_type                    = var.vm_type == "linux-scale-set" ? "Linux" : "Windows"
-  nessus_server              = var.nessus_server
-  nessus_key                 = var.nessus_key
-  nessus_groups              = var.nessus_groups
-  install_dynatrace_oneagent = var.install_dynatrace_oneagent
-  install_azure_monitor      = var.install_azure_monitor
-  install_nessus_agent       = var.nessus_install
+  virtual_machine_type         = "vmss"
+  virtual_machine_scale_set_id = var.vm_type == "linux-scale-set" ? azurerm_linux_virtual_machine_scale_set.linux_scale_set[0].id : azurerm_windows_virtual_machine_scale_set.windows_scale_set[0].id
+  splunk_username              = var.splunk_username
+  splunk_password              = var.splunk_password
+  splunk_pass4symmkey          = var.splunk_pass4symmkey
+  splunk_group                 = var.splunk_group
+  os_type                      = var.vm_type == "linux-scale-set" ? "Linux" : "Windows"
+  nessus_server                = var.nessus_server
+  nessus_key                   = var.nessus_key
+  nessus_groups                = var.nessus_groups
+  install_dynatrace_oneagent   = var.install_dynatrace_oneagent
+  install_azure_monitor        = var.install_azure_monitor
+  install_nessus_agent         = var.nessus_install
 
   dynatrace_hostgroup = var.dynatrace_hostgroup
   dynatrace_server    = var.dynatrace_server
