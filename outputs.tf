d0ce784a8994b3257086ce4428fd774f90771f7d
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-07-03 15:41:53 +0100

    updated to test output value

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,7 @@ output "vm_id" {
 }
 
 output "principal_id" {
-  value = var.vm_type == "windows-scale-set" ? azurerm_windows_virtual_machine_scale_set.windows_scale_set[0].identity.0.principal_id : var.vm_type == "linux-scale-set" ? azurerm_linux_virtual_machine_scale_set.linux_scale_set[0].identity.0.principal_id : null
+  value = azurerm_windows_virtual_machine_scale_set.windows_scale_set[0].identity.0.principal_id
+
+  # value = var.vm_type == "windows-scale-set" ? azurerm_windows_virtual_machine_scale_set.windows_scale_set[0].identity.0.principal_id : var.vm_type == "linux-scale-set" ? azurerm_linux_virtual_machine_scale_set.linux_scale_set[0].identity.0.principal_id : null
 }
