0061f23322ae92733e18588bea69d8d73717ecdf
Johns293 <Johns293@user.noreply.github.com>
2023-10-27 15:53:55 +0100

    comment provider

--- extensions.tf
+++ extensions.tf
@@ -1,10 +1,10 @@
 
 module "vm-bootstrap" {
-  providers = {
+  /* providers = {
     azurerm.cnp = azurerm.cnp
     azurerm.soc = azurerm.soc
     azurerm     = azurerm
-  }
+  } */
 
   count                        = var.install_splunk_uf == true || var.nessus_install == true ? 1 : 0
   source                       = "git::https://github.com/hmcts/terraform-module-vm-bootstrap.git?ref=master"
