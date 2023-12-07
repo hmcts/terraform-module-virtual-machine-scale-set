101b6d6dd6cd67aaa10c9f6e0059f0faddc799da
Johns293 <Johns293@user.noreply.github.com>
2023-12-07 12:48:50 +0000

    added plan to the pipeline

--- example/tests/module/setup/main.tf
+++ example/tests/module/setup/main.tf
@@ -27,3 +27,8 @@ resource "azurerm_subnet" "test" {
   virtual_network_name = azurerm_virtual_network.test.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+provider "azurerm" {
+  features {}
+}
+  
