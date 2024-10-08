2d6ec05a47a58aa1d92a5201bd8cb5c16f476aff
Chirag Pareek <111435448+cpareek@users.noreply.github.com>
2024-10-08 16:49:41 +0100

    Update extensions.tf

--- extensions.tf
+++ extensions.tf
@@ -4,6 +4,7 @@ module "vm-bootstrap" {
     azurerm     = azurerm
     azurerm.cnp = azurerm.cnp
     azurerm.soc = azurerm.soc
+    azurerm.dcr = azurerm.dcr
   }
 
   count                        = var.install_splunk_uf == true || var.nessus_install == true ? 1 : 0
